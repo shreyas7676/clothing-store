b16fe7c1c092d741fd64a3587ca1055367c39461
shreyas7676 <shreep7676@gmail.com>
2020-01-22 08:00:11 +0530

    shop page is created and browserrouter router package added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </div>
     );
   }
